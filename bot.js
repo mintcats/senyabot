5ccc426072d8f9782fe805f751b041a10b2f3d21
mintcats <38986750+mintcats@users.noreply.github.com>
2018-05-06 16:36:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,17 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-var prefix = '!'
+var prefix = '!';
+var peoples = ['Сеня', 'Лера', 'Владик', 'Влад', 'Джэ', 'Настя'];
+var number = 0;
 
 client.on('message', message => {
     if(message.author === client.user) return;
-    if(message.content.startsWith(prefix + 'help')) {
-        message.channel.sendMessage('Привет я БОТ!');
-     }
-});
+    if(message.content.startsWith(prefix + 'add'))   {var message_split = message.content.split(' '); peoples.push(message_split[1]); message.channel.sendMessage(peoples);};
+    if(message.content.startsWith(prefix + 'who'))   {message.channel.sendMessage(peoples[number]);};
+    if(message.content.startsWith(prefix + 'list'))  {message.channel.sendMessage(peoples);};
+    if(message.content.startsWith(prefix + 'next'))  {number = number+1; if(number > peoples.length){number = 0;}; message.channel.sendMessage(peoples[number]); };
+    if(message.content.startsWith(prefix + 'remove')){var message_split = message.content.split(' '); var index = peoples.indexOf(message_split[1]); peoples.splice(index,1); message.channel.sendMessage(peoples);};
+    });
 
 client.login('NDQxOTc3MDI5MTMxNTAxNTY4.Dc4HsQ.90Lpo4xC3oPxF2aFNEtmVU0EmMo');
